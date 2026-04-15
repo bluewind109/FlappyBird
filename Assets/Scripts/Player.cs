f79acf4896e382452920933e1a838fbd1869395d
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-04-15 23:05:21 +0700

    * Animate player's sprite

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,11 +2,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex = 0;
+
     private Vector3 direction;
     public float gravity = -9.8f;
     public float jumpStrength = 5f;
 
     private const int LEFT_CLICK = 0;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex = (spriteIndex + 1) % sprites.Length;
+        spriteRenderer.sprite = sprites[spriteIndex];
+    }
 
     void Update()
     {
